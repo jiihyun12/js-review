883534475d75338f0001b64874e4c0cc8c6b0439
jiihyun12 <j22h.h0h@gmail.com>
2025-11-20 22:03:34 +0900

    251120_

---
+++ JS_Daily_Schedule/day01/app.js
@@ -0,0 +1,33 @@
+
+const schedules = [
+  {
+    id: 1,
+    group: "SC-01",
+    start: "2025-11-20 09:00",
+    end:   "2025-11-20 10:30",
+  },
+  {
+    id: 2,
+    group: "SC-02",
+    start: "2025-11-20 08:00",
+    end:   "2025-11-20 09:00",
+  },
+  {
+    id: 3,
+    group: "SC-03",
+    start: "2025-11-20 10:00",
+    end:   "2025-11-20 11:00",
+  },
+];
+
+// for(let i = 0; i < schedules.length; i++){
+//    const schedule = schedules[i]
+//    console.log(`[${schedule["id"]}] 그룹 : ${schedule["group"]} 시간 : ${schedule["start"]} ~ ${schedule["end"]}`)
+//}
+
+for (let i = 0; i < schedules.length; i++){
+  const schedule = schedules[i]
+  if(schedule["group"] === "SC-03"){
+    console.log(schedule)
+  }
+}
