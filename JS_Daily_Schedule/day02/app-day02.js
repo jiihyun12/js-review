96635db243691659f35b9dbb84c6abcf940bf91e
jiihyun12 <j22h.h0h@gmail.com>
2025-11-21 12:52:52 +0900

    251121

--- JS_Daily_Schedule/day02/app-day02.js
+++ JS_Daily_Schedule/day02/app-day02.js
@@ -20,12 +20,30 @@ const schedules = [
 ];
 
 
+// 각 스케줄의 시간 차이(분 단위)를 계산해서 출력
 
 for (let i = 0; i < schedules.length; i++){
   const schedule = schedules[i]
-  if (schedule["group"] === "SC-02"){
-    continue
+  const start = schedule["start"]
+  const date = start.split(" ")
+  const hour = date[1]
+  const minute = hour.split(":")
+  const h = parseInt(minute[0])
+  const m = parseInt(minute[1])
+  const startTotal = h * 60 + m
+
+  const end = schedule["end"]
+  const end_date = end.split(" ")
+  const end_hour = end_date[1]
+  const end_minute = end_hour.split(":")
+  const end_h = parseInt(end_minute[0])
+  const end_m = parseInt(end_minute[1])
+  const endTotal = end_h * 60 + end_m
+
+  result = endTotal - startTotal
+  console.log(`${schedule["group"]}의 시간차 : ${result}`)
   }
-  console.log(schedule)
-}
+
+
+
 
